5722f2948507ff41457c9236de5b7b606a181927
Martin Serik <59570728+syringus@users.noreply.github.com>
2025-07-11 21:08:58 +0200

    Add miryoku media, button, navigation and symbols layers

--- keyboards/geigeigeist/totem/keymaps/default/keymap.c
+++ keyboards/geigeigeist/totem/keymaps/default/keymap.c
@@ -41,25 +41,53 @@ enum totem_layers {
 #define LT_ESC LT(U_MEDIA, KC_ESC)
 #define LT_SPC LT(U_NAV, KC_SPC)
 #define LT_TAB LT(U_MOUSE, KC_TAB)
+#define LT_BSPC LT(U_NUM, KC_BSPC)
+#define LT_ENT LT(U_SYM, KC_ENT)
+#define LT_Z LT(U_BUTTON, KC_Z)
+#define LT_SLSH LT(U_BUTTON, KC_SLSH)
 
 // LAYERS
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [U_BASE] = LAYOUT(
-                 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-                 MT_A,    MT_S,    MT_D,    MT_F,    KC_G,                         KC_H,    MT_J,    MT_K,    MT_L,    MT_SCLN,
-        KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMM, MT_DOT,  KC_SLSH, KC_NO,
-                                            KC_ESC,  LT_SPC, LT_TAB,     KC_ENT,   KC_BSPC, KC_DEL
+                 KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+                 MT_A,    MT_S,    MT_D,    MT_F,    KC_G,                          KC_H,    MT_J,    MT_K,    MT_L,    MT_SCLN,
+        KC_NO,   LT_Z,    MT_X,    KC_C,    KC_V,    KC_B,                          KC_N,    KC_M,    KC_COMM, MT_DOT,  LT_SLSH, KC_NO,
+                                            LT_ESC,  LT_SPC,  LT_TAB,     LT_ENT,   LT_BSPC, KC_DEL
     ),
     [U_NAV] = LAYOUT(
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                        KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,CW_TOGG,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_NO,
-                                            KC_NO,   KC_NO,  KC_NO,      KC_ENT,   KC_BSPC, KC_DEL
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                         KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,CW_TOGG,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,  KC_NO,
+                                            KC_NO,   KC_NO,   KC_NO,      KC_ENT,   KC_BSPC, KC_DEL
     ),
     [U_MOUSE] = LAYOUT(
-                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                        MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_NO,
-        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                        MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_NO,   KC_NO,
-                                            KC_NO,   KC_NO,  KC_NO,      MS_BTN2,  MS_BTN1, MS_BTN3
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                         MS_LEFT, MS_DOWN, MS_UP,   MS_RGHT, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         MS_WHLL, MS_WHLD, MS_WHLU, MS_WHLR, KC_NO,   KC_NO,
+                                            KC_NO,   KC_NO,   KC_NO,      MS_BTN2,  MS_BTN1, MS_BTN3
+    ),
+    [U_MEDIA] = LAYOUT(
+                 KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                         KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                            KC_NO,   KC_NO,   KC_NO,      KC_MSTP,  KC_MPLY, KC_MUTE
+    ),
+    [U_BUTTON] = LAYOUT(
+                 U_UND,   U_CUT,   U_CPY,   U_PST,   U_RDO,                         U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
+                 KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_NO,                         KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_NO,   U_UND,   U_CUT,   U_CPY,   U_PST,   U_RDO,                         U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,   KC_NO,
+                                            MS_BTN3, MS_BTN1, MS_BTN2,    MS_BTN2,  MS_BTN1, MS_BTN3
+    ),
+    [U_NUM] = LAYOUT(
+                 KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,                        KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_NO,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                            KC_DOT,  KC_0,    KC_MINS,    KC_NO,    KC_NO,   KC_NO
+    ),
+    [U_SYM] = LAYOUT(
+                 KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                 KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                       KC_NO,   KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+        KC_NO,   KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,                       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+                                            KC_LPRN, KC_RPRN, KC_UNDS,    KC_NO,    KC_NO,   KC_NO
     )
 };
