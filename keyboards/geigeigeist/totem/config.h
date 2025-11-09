574f280baf63cedd5c25aea3d60a461eab9ab8e4
Martin Serik <59570728+syringus@users.noreply.github.com>
2025-11-09 17:21:59 +0100

    Add Flow Tap
    https://docs.qmk.fm/tap_hold#flow-tap

--- keyboards/geigeigeist/totem/config.h
+++ keyboards/geigeigeist/totem/config.h
@@ -10,3 +10,5 @@
 
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET // Activates the double-tap behavior
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U // Timeout window in ms in which the double tap can occur
+
+#define FLOW_TAP_TERM 150
