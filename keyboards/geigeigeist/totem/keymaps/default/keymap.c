2a6d4574920dfab449a24d475fd2d2540115bcbc
Martin Serik <59570728+syringus@users.noreply.github.com>
2025-11-23 20:38:13 +0100

    Replace LT_GAME2 with MO_GAME2

--- keyboards/geigeigeist/totem/keymaps/default/keymap.c
+++ keyboards/geigeigeist/totem/keymaps/default/keymap.c
@@ -53,7 +53,6 @@ enum layers {
 #define LT_DEL LT(U_FUN, KC_DEL)
 #define LT_Z LT(U_BUTTON, KC_Z)
 #define LT_SLSH LT(U_BUTTON, KC_SLSH)
-#define LT_GAME2 LT(U_GAME2, KC_ESC)
 #define MO_GAME2 MO(U_GAME2)
 // Tap dance
 #define TD_BASE TD(TD_U_BASE)
@@ -182,12 +181,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TAB,  KC_Q,    KC_V,    KC_E,    KC_R,                          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TAB,
                  KC_LSFT, KC_A,    KC_W,    KC_D,    KC_F,                          KC_NO,   KC_LEFT, KC_UP,   KC_RIGHT,KC_LSFT,
         KC_ESC,  KC_LCTL, KC_Z,    KC_S,    KC_X,    KC_C,                          KC_NO,   KC_NO,   KC_DOWN, KC_NO,   KC_LCTL, TD_BASE,
-                                            KC_LALT, KC_SPC,  LT_GAME2,   MO_GAME2, KC_SPC,  KC_LALT
+                                            KC_LALT, KC_SPC,  MO_GAME2,   MO_GAME2, KC_SPC,  KC_LALT
     ),
     [U_GAME2] = LAYOUT(
                  KC_HOME, KC_J,    KC_I,    KC_M,    KC_T,                          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
                  KC_ENT,  KC_4,    KC_5,    KC_6,    KC_G,                          KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT,
         KC_H,    KC_BSPC, KC_1,    KC_2,    KC_3,    KC_B,                          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LGUI,
-                                            KC_NO,   KC_NO,   KC_NO,      MO_GAME2, KC_BSPC, KC_DEL
+                                            KC_NO,   KC_NO,   MO_GAME2,   MO_GAME2, KC_BSPC, KC_DEL
     )
 };
